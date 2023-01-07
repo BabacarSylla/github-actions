9a2ba7b9f0190be4a6a955c5ad3d33e4fe71bb22
BabacarSylla <babisko9817@gmail.com>
2023-01-07 08:32:10 +0000

    code

---
+++ src/main/java/com/groupeisi/cicd/GithubCicdActionsApplication.java
@@ -0,0 +1,30 @@
+package com.groupeisi.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GithubCicdActionsApplication {
+
+	@GetMapping("/welcome")
+	public String Welcome(){
+		return "Bienvenue sur Github CI/CD Actions Application !";
+	}
+
+
+	//echo "# github-actions" >> README.md
+	//git init
+	//git add README.md
+	//git commit -m "first commit"
+	//git branch -M main
+	//git remote add origin https://github.com/BabacarSylla/github-actions.git
+	//git push -u origin main
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
+	}
+
+}
